ce0d82f91ad3232acd902c882da9ce416b3b023a
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-08-03 13:56:07 +0200

    Updated OpenAI model list

--- MODEL_TYPES.py
+++ MODEL_TYPES.py
@@ -1,4 +1,4 @@
 DEFAULT_EMBED_MODEL = 'text-embedding-3-small'
 LLM_MODELS = [
-    'gpt-3.5-turbo', 'gpt-3.5-turbo-instruct', 'gpt-4-turbo', 'gpt-4-turbo-preview', 'gpt-4', 'gpt-4-32k' , 
+    'gpt-3.5-turbo', 'gpt-3.5-turbo-instruct', 'gpt-4-turbo', 'gpt-4-turbo-preview', 'gpt-4', 'gpt-4-32k', 'gpt-4o', 'gpt-4o-mini', 
 ]
