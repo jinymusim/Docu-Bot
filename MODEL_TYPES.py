b8aefd6e2b5d2029dcc910323a8da5d34f14b69a
Michal Chudoba <michal.chudoba.praha@gmail.com>
2024-11-20 17:32:24 +0100

    Better model names

--- MODEL_TYPES.py
+++ MODEL_TYPES.py
@@ -1,4 +1,4 @@
-DEFAULT_EMBED_MODEL = 'bigger_vectors'
+DEFAULT_EMBED_MODEL = 'all-mpnet-base-v1'
 DEFAULT_EMBED_LOC = "http://localhost:5001/v1"
 LLM_MODELS = {
     'gpt-3.5-turbo' : None, 
@@ -6,6 +6,6 @@ LLM_MODELS = {
     'gpt-4o' : None, 
     'gpt-4o-mini' : None, 
     "o1-mini" : None, 
-    "small_llm" : "http://localhost:5000/v1",
-    "bigger_llm" : "http://localhost:5000/v1",
+    "Llama-3.2-1B" : "http://localhost:5000/v1",
+    "Llama-3.2-3B" : "http://localhost:5000/v1",
 }
